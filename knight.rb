bdd12a1b167f2b49607590a7eb317f6b9b335f03
Dom <dominykas.laurinavicius@gmail.com>
2019-07-13 17:49:20 +0300

    Add first draft of build_move_tree

--- knight.rb
+++ knight.rb
@@ -2,7 +2,7 @@ require_relative 'polytree.rb'
 
 class KnightPathFinder
     def initialize(pos)
-        @root_node = PolyTreeNode(pos)
+        @root_node = PolyTreeNode.new(pos)
         @considered_positions = [pos]
         self.build_move_tree
     end
@@ -26,27 +26,23 @@ class KnightPathFinder
     end
 
     def build_move_tree
-        
         move_tree = []
-        queue = new_move_positions(@root_node)
+        queue = [@root_node]
 
         until queue.empty?
-
+            position = queue.shift
+            new_nodes = new_move_positions(position).map do |pos| 
+                node = PolyTreeNode.new(pos)
+                node.parent = @root_node
+                node
+            end
+            move_tree << new_nodes
+            queue.concat(new_nodes)
         end
-
-        #use new_move_positions(pos)
-        # use the bfs idea
-        # use a queue to process nodes in FIFO order
-        # start with root_node representing the start_pos
-        explore moves from one position at a time
-        next build nodes represting positions one move away, add these to the queue
-        then take the next node from the queue ... until the queue is empty.
+        move_tree
     end
 
-    to help us find paths - build a move tree 
-    values in the tree will be positions
-    Use PolyTreeNode instances to represent each position
-
+        
 end
 
 
@@ -75,3 +71,19 @@ plan:
     #call #self.valid_moves(pos) but filter out any positions already in @considered_positions
     #add remaining new positions to @considered_positions
     #return these new positions
+    
+    #use new_move_positions(pos)
+    # use the bfs idea
+    # use a queue to process nodes in FIFO order
+    # start with root_node representing the start_pos
+
+    # explore moves from one position at a time
+        # next build nodes represting positions one move away, add these to the queue
+        # then take the next node from the queue ... until the queue is empty.
+
+    # to help us find paths - build a move tree 
+    # values in the tree will be positions
+    # Use PolyTreeNode instances to represent each position
+
+    # a node is child of another node if you can move from the parent position directly to the child position.
+    # root is knight's' starting position.
