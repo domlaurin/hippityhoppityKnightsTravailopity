61e8d9f5f82834e281944266e15ffba461dc8869
Dom <dominykas.laurinavicius@gmail.com>
2019-07-13 17:32:28 +0300

    Write a better plan

--- knight.rb
+++ knight.rb
@@ -26,22 +26,33 @@ class KnightPathFinder
     end
 
     def build_move_tree
-        array = [@root_node]
+        
+        move_tree = []
+        queue = new_move_positions(@root_node)
 
-        until array.empty?
-            node = array.shift
+        until queue.empty?
+
         end
 
         #use new_move_positions(pos)
-        #use the bfs idea
-        #use a queue to process nodes in FIFO order
-        #start with root_node representing the start_pos
-        #explore moves from one position at a time
-        #next build nodes represting positions one move away, add these to the queue
-        #then take the next node from the queue ... until the queue is empty.
+        # use the bfs idea
+        # use a queue to process nodes in FIFO order
+        # start with root_node representing the start_pos
+        explore moves from one position at a time
+        next build nodes represting positions one move away, add these to the queue
+        then take the next node from the queue ... until the queue is empty.
     end
 
+    to help us find paths - build a move tree 
+    values in the tree will be positions
+    Use PolyTreeNode instances to represent each position
+
 end
+
+
+
+
+
 def bfs(target_value)
     array = [self]
     until array.empty?
