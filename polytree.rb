4f8b138ac1cf1964e870bd1ed8ed538463a73fb0
Dom <dominykas.laurinavicius@gmail.com>
2019-07-13 13:02:36 +0300

    Initial commit

---
+++ polytree.rb
@@ -0,0 +1,62 @@
+require 'byebug'
+class PolyTreeNode
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(node)
+        unless @parent == nil
+            @parent.children.delete(self)
+        end
+        @parent = node
+        unless node == nil
+            node.children << self
+        end
+    end
+
+    def add_child(child_node)
+        child_node.parent = self
+    end
+
+    def remove_child(child_node)
+        raise "error" if child_node.parent == nil
+        child_node.parent = nil
+    end
+
+    def dfs(target_value)
+        return self if self.value == target_value
+
+        self.children.each do |child|
+            node = child.dfs(target_value)
+            if node == nil
+                next
+            else
+                return node
+            end
+        end
+        nil
+    end
+
+    def bfs(target_value)
+        array = [self]
+        until array.empty?
+            boy = array.shift
+            if boy.value == target_value
+                return boy
+            else
+                array << boy.children
+                array = array.flatten
+            end
+        end
+    end
+
+    def inspect
+        @value.inspect
+    end
+
+attr_reader :parent, :children, :value
+end
+
+
