c46bfc9c367401f17ea4c289e149dbce16a5e0e4
Dom <dominykas.laurinavicius@gmail.com>
2019-07-14 17:56:01 +0300

    Add proper plan for Phase I

--- plan.rb
+++ plan.rb
@@ -1,9 +1,27 @@
-Plan:
-1. You will want to build on your PolyTreeNode work, using PolyTreeNode instances to represent each position.
+1. Create KnightPathFinder class
+    a. let the initialize method accept a starting pos
+    b. set @root_node = starting pos
+    c. set @move_tree = "baobab" #should have .length 64
+    d. set @considered_positions = [@root_node]
+    d. call #build_move_tree
+
+2. make ::valid_moves(pos)
+    a. find up to 8 possible moves
+
+3. make #new_move_positions(pos)
+    a. call ::valid_moves(pos)
+    b. remove any moves that were already in @considered_positions
+    c. add remaining new moves to @considered_positions
+    d. return new moves
 
 
+build a move tree
+    a. the values will be positions (use PolyTreeNode instances to represent each pos)
+    b. set node's' children as its children.
+    c. the root node should be the starting pos
 
-#find_path
+ beginning with @root_node, create #build_move_tree
 
-create class method valid_moves(pos)
+ I will traverse @move_tree whenever I call #find_path
+
  
