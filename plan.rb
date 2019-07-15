bd0171b1372ef058933fd8f4051bc09e0589c7d0
Dom <dominykas.laurinavicius@gmail.com>
2019-07-15 11:01:06 +0300

    Add plan bits from knight.rb

--- plan.rb
+++ plan.rb
@@ -25,3 +25,37 @@ build a move tree
  I will traverse @move_tree whenever I call #find_path
 
  
+
+# plan:
+    #up to 8 possible moves
+
+    #call #self.valid_moves(pos) but filter out any positions already in @considered_positions
+    #add remaining new positions to @considered_positions
+    #return these new positions
+    
+    #use new_move_positions(pos)
+    # use the bfs idea
+    # use a queue to process nodes in FIFO order
+    # start with root_node representing the start_pos
+
+    # explore moves from one position at a time
+        # next build nodes represting positions one move away, add these to the queue
+        # then take the next node from the queue ... until the queue is empty.
+
+    # to help us find paths - build a move tree 
+    # values in the tree will be positions
+    # Use PolyTreeNode instances to represent each position
+
+    # a node is child of another node if you can move from the parent position directly to the child position.
+    # root is knight's' starting position.
+
+    # search for end_pos in the move_tree
+    # use either dfs or bfs from the PolyTreeNode exercises, it doensn't' matter
+    # this should return the tree node instance containing end_pos
+
+    # create #trace_path_back
+    # this should trace back from node to the root using PolyTreeNode#parent
+    # as it goes up and up toward the root - it should add the value to an array
+
+    # trace_path_back should return the values in order from the start position to the end position
+    # use trace_path_back to finish up find_path
